fe17eaaa3928d8630c0a1d0b7bf714d8fc7f4090
harsh1812003 <h9773129201@gmail.com>
2024-06-20 12:30:10 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from text.logging import logger
 from text.pipeline.stage_01_pipeline import DataIngestionTrainingPipeline
+from text.pipeline.stage_02_pipeline import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
