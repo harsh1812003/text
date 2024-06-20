cd1feb812175c9ab1b2424a3ecacd05462989f2d
harsh1812003 <h9773129201@gmail.com>
2024-06-20 11:29:17 +0530

    DataIngestion

---
+++ main.py
@@ -0,0 +1,12 @@
+from text.logging import logger
+from text.pipeline.stage_01_pipeline import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
